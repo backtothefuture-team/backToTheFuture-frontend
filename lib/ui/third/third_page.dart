ded0365e010e34342127e2d87e2d82f5cad9d4bc
nala0897@naver.com <nala0897@naver.com>
2024-03-06 19:36:27 +0900

    feat: third page 에 들어갈 화면 구성 샘플

---
+++ lib/ui/third/third_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api_ex/ui/third/third_menu_list.dart';
+import 'package:rest_api_ex/ui/third/third_user_profile.dart';
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('ThirdPage'),
+      ),
+
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            UserProfile(),
+            MenuList()
+          ],
+        ),
+      ),
+    );
+  }
+}
