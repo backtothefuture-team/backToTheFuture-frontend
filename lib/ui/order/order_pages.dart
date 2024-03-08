641235fab2c667ef09a9d81a91014d6fec206775
nala0897@naver.com <nala0897@naver.com>
2024-03-08 23:44:23 +0900

    fix: 스크롤 뷰로 수정

--- lib/ui/order/order_pages.dart
+++ lib/ui/order/order_pages.dart
@@ -19,18 +19,24 @@ class OrderPages extends StatelessWidget {
 
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-
-            // 픽업 시간 정보
-            const PickUpTimeInfo(),
-
-            // 예약 시 유의사항
-            reservationNotice(),
-
-            // 결제할 상품 목록
-            const OrderList()
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+          
+              // 픽업 시간 정보
+              const PickUpTimeInfo(),
+          
+              Divider(color: Colors.grey),
+          
+              // 예약 시 유의사항
+              reservationNotice(),
+          
+              Divider(color: Colors.grey,),
+          
+              // 결제할 상품 목록
+              const OrderList()
+            ],
+          ),
         ),
       ),
     );
@@ -39,13 +45,17 @@ class OrderPages extends StatelessWidget {
 
   // 예약 시 유의사항
   Widget reservationNotice() {
-    return const Column(
-      children: [
-        Text('예약 시 유의사항'),
-        Text('1. 가게 재고 상황에 따라 예약이 취소될 수 있습니다. 예약 취소시 결제 금액은 100% 환불됩니다.'),
-        Text('2. 예약이 확정되면, 희망 픽업시간 내 픽업 부탁드립니다'),
-        Text('3. 예약 확정 후 픽업이 이뤄지지 않으면 환불 조치는 불가합니다.')
-      ],
+    return const Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('예약 시 유의사항'),
+          Text('1. 가게 재고 상황에 따라 예약이 취소될 수 있습니다. 예약 취소시 결제 금액은 100% 환불됩니다.'),
+          Text('2. 예약이 확정되면, 희망 픽업시간 내 픽업 부탁드립니다'),
+          Text('3. 예약 확정 후 픽업이 이뤄지지 않으면 환불 조치는 불가합니다.')
+        ],
+      ),
     );
   }
 }
