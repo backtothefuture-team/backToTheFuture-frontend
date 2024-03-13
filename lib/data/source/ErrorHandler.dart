24d5f548642448f1b4e92638b1a6fbdc6d1e348a
nala0897@naver.com <nala0897@naver.com>
2024-03-13 20:54:03 +0900

    feat: Dio ErrorHandler 클래스 생성

--- lib/data/source/ErrorHandler.dart
+++ lib/data/source/ErrorHandler.dart
@@ -1,12 +1,37 @@
 import 'package:dio/dio.dart';
 
 class ErrorHandler implements Exception {
+  late String failure;
 
   ErrorHandler.handle(dynamic error) {
     if( error is DioException ) {
-      print('DioException');
+      failure = _handleError(error);
+      print('DioException >> $failure');
     } else {
       print('error');
     }
+  }
+}
+
+String _handleError(DioException error) {
+  switch( error.type ) {
+    case DioExceptionType.connectionError:
+      return 'connectionError';
+    case DioExceptionType.connectionTimeout:
+      return 'connectionTimeout';
+    case DioExceptionType.sendTimeout:
+      return 'sendTimeout';
+    case DioExceptionType.receiveTimeout:
+      return 'receiveTimeout';
+    case DioExceptionType.badCertificate:
+    return 'badCertificate';
+    case DioExceptionType.badResponse:
+    return 'badResponse';
+    case DioExceptionType.cancel:
+    return 'cancel';
+    case DioExceptionType.unknown:
+    return 'unknown';
+    default:
+      return 'default_error';
   }
 }
