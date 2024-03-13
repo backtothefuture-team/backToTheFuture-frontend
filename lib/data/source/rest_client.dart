1f9b95f4055fb91d6ddd697f58d8e75b393ec503
nala0897@naver.com <nala0897@naver.com>
2024-03-13 19:54:35 +0900

    feat: RestClient 클래스에 createUser 생성

--- lib/data/source/rest_client.dart
+++ lib/data/source/rest_client.dart
@@ -11,4 +11,13 @@ abstract class RestClient {
 
   @GET('/member/register/{id}')
   Future<UserModel> getUser({@Path() required int id});
+
+  @POST('/member/register')
+  Future<UserModel> createUser(
+    @Field('name') String name,
+    @Field('email') String email,
+    @Field('password') String password,
+    @Field('passwordConfirm') String passwordConfirm,
+    @Field('phoneNumber') List<String> phoneNumber,
+  );
 }
