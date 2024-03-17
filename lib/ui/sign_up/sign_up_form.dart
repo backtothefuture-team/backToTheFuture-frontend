ded033477ac9db17a24f0821309a23185f216b7a
NalaJang <73895803+NalaJang@users.noreply.github.com>
2024-03-17 16:49:00 +0900

    feat: sign Up UI 2 (#22)
    * fix: 유효성 검사 코드 추가
    
    * fix: 이메일 인증 시 사용하는 아이콘 추가 및 focusedBorder 추가
    
    * fix: 개별 함수로 구현 해놓은 validate코드 적용
    
    * fix: 개별 함수로 구현 해놓은 validate코드 적용
    
    * feat: 이메일 인증 코드 입력 UI 구현
    
    * feat: 이용약관 UI 구현
    
    * feat: 가입 버튼 UI 구현
    
    * fix: SignUp class 에 SignUpButton() 추가
    
    * feat: 이메일 회원 가입 UI 구현

--- lib/ui/sign_up/sign_up_form.dart
+++ lib/ui/sign_up/sign_up_form.dart
@@ -1,16 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:rest_api_ex/data/model/user_model.dart';
-import 'package:rest_api_ex/data/source/ErrorHandler.dart';
-import 'package:rest_api_ex/data/source/rest_client.dart';
+import 'package:rest_api_ex/config/gaps.dart';
+import 'package:rest_api_ex/config/inputValidation.dart';
 import 'package:rest_api_ex/config/user_info_text_form_field.dart';
 
-import '../../config/my_validator.dart';
+import '../../config/palette.dart';
+import 'term_of_use.dart';
 
 class SignUpForm extends StatelessWidget {
-  SignUpForm({
+  const SignUpForm({
     required this.formKey,
-    required this.userNameController,
+    required this.userNickNameController,
+    required this.userEmailController,
     required this.userPasswordController,
     required this.userMobileNumberController,
     required this.userResidenceController,
@@ -20,14 +21,13 @@ class SignUpForm extends StatelessWidget {
 
   final GlobalKey<FormState> formKey;
 
-  final TextEditingController userNameController;
+  final TextEditingController userNickNameController;
+  final TextEditingController userEmailController;
   final TextEditingController userPasswordController;
   final TextEditingController userMobileNumberController;
   final TextEditingController userResidenceController;
   final TextEditingController promotionCodeController;
   final VoidCallback onSignUpPressed;
-
-  final RestClient restClient = GetIt.instance<RestClient>();
 
   @override
   Widget build(BuildContext context) {
@@ -35,83 +35,88 @@ class SignUpForm extends StatelessWidget {
       key: formKey,
       child: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.9,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-
-              // 이름
-              UserInfoTextFormField(
-                controller: userNameController,
-                validator: (value) => myValidate(value, '이름'),
-                decorationLabelText: '이름',
+              // 이메일
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('이메일'),
+                  Gaps.gapH5,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'test@email.com',
+                      labelStyle: TextStyle(
+                        color: Colors.black54,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Palette.disabledColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-
 
               // 비밀번호
-              UserInfoTextFormField(
-                controller: userPasswordController,
-                validator: (value) => myValidate(value, '비밀번호', minLength: 6),
-                decorationLabelText: '비밀번호',
+              Column(
+                children: [
+                  const Row(
+                    children: [
+                      Text('비밀번호'),
+                      Text('*', style: TextStyle(color: Colors.red)),
+                    ],
+                  ),
+
+                  Gaps.gapH5,
+
+                  // 비밀번호
+                  UserInfoTextFormField(
+                    controller: userPasswordController,
+                    validator: validatePassword,
+                    decorationLabelText: '비밀번호',
+                  ),
+
+                  Gaps.gapH10,
+
+                  // 비밀번호 확인
+                  UserInfoTextFormField(
+                    controller: null,
+                    validator: (value) => validateConfirmPassword(
+                        value, userPasswordController.text),
+                    decorationLabelText: '비밀번호 확인',
+                  ),
+                ],
               ),
 
-
-              // 비밀번호 확인
-              UserInfoTextFormField(
-                controller: null,
-                validator: (value) => myValidate(value, '비밀번호', confirmPassword: true),
-                decorationLabelText: '비밀번호 확인',
+              // 닉네임
+              Column(
+                children: [
+                  const Row(
+                    children: [
+                      Text('닉네임'),
+                      Text('*', style: TextStyle(color: Colors.red)),
+                    ],
+                  ),
+                  Gaps.gapH5,
+                  UserInfoTextFormField(
+                    controller: userNickNameController,
+                    validator: validateNickName,
+                    decorationLabelText: '닉네임을 입력해 주세요',
+                  ),
+                ],
               ),
 
-
-              // 휴대전화번호
-              UserInfoTextFormField(
-                controller: userMobileNumberController,
-                validator: (value) => myValidate(value, '휴대전화번호'),
-                decorationLabelText: '휴대전화번호',
-              ),
-
-
-              // 거주지
-              UserInfoTextFormField(
-                controller: userResidenceController,
-                validator: (value) => myValidate(value, '거주지'),
-                decorationLabelText: '거주지',
-              ),
-
-
-
-              // 프로모션 코드
-              UserInfoTextFormField(
-                controller: promotionCodeController,
-                validator: (value) => null,
-                decorationLabelText: '프로모션 코드',
-              ),
-
-
-              // 회원가입 버튼
-              ElevatedButton(
-                onPressed: () async {
-                  List<String> phoneNumber = ['010', '5678', '1234'];
-                  var userModel = UserModel(
-                      name: 'test1',
-                      email: 'test1@email.com',
-                      password: '123456',
-                      passwordConfirm: '123456',
-                      phoneNumber: phoneNumber);
-
-                  try {
-                    await restClient.createUser(userModel);
-
-                  } catch(error) {
-                    ErrorHandler.handle(error);
-                  }
-                },
-                child: Text('회원가입')
-              )
+              // 이용약관 동의
+              const TermOfUse(),
             ],
           ),
         ),
-      )
+      ),
     );
   }
 }
