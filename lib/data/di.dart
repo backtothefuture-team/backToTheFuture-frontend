392531f9b0954cd594c6fe249a0d50368a80162b
nala0897@naver.com <nala0897@naver.com>
2024-03-20 11:52:28 +0900

    feat: 카카오 로그인 초기화 설정

--- lib/data/di.dart
+++ lib/data/di.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:rest_api_ex/data/source/rest_client.dart';
 
 final instance = GetIt.instance;
@@ -9,4 +11,12 @@ Future<void> initModule() async {
   final dio = Dio();
 
   instance.registerLazySingleton(() => RestClient(dio));
+
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '81744e25b42accb8e343708484a15cda',
+    javaScriptAppKey: '80398c7b09ada91d124f99e1503edfa2',
+  );
 }
